34cc6a2c826ab7d07c0d0e28d65554904c174f9a
arnie-silva <soxrbs@icloud.com>
2025-05-14 19:22:13 -0700

    Added Regression Model Predictions
    Added PyTorch regression model and ignored venv from tracking

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -2,8 +2,20 @@ from fastapi import APIRouter
 from models.schemas import Item
 from fetch_data import df
 from fastapi.responses import JSONResponse
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 router = APIRouter()
+
+class EnergyPredictor(nn.Module):
+        def __init__(self):
+                super(EnergyPredictor, self).__init__()
+                self.linear = nn.Linear(1,1)
+        
+        def forward(self, x):
+                return self.linear(x)
+model = EnergyPredictor
 
 @router.get("/")
 def read_root():
@@ -21,6 +33,26 @@ def read_state(state_id: str):
     })
     data_json = filtered_df.to_dict(orient='records')
     return JSONResponse(content=data_json)
+
+@router.get("/predict/{state_id}/{year}")
+def predict(state_id: str, year: int):
+    X = torch.tensor(df[(df['fueltypeid'] == 'ALL') & (df['location'] == state_id)]['year'].values, dtype=torch.float32)
+    y = torch.tensor(df[(df['fueltypeid'] == 'ALL') & (df['location'] == state_id)]['total-consumption-btu'].values, dtype=torch.float32)
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+    for epoch in range(100):
+        model.train()
+        output = model(X)
+        loss = criterion(output, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    with torch.no_grad():
+        input_tensor = torch.tensor([[year]], dtype=torch.float32)
+        prediction = model(input_tensor).item()
+    return {"predicted_value": prediction}
 
 @router.post("/items/")
 def create_item(item: Item):
